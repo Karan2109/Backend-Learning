45912cdf0bff60347a9d895e4397b5ee7ea0c034
Karan Singh <karans21.dev@gmail.com>
2025-05-17 12:17:38 +0530

    syntax error explained

---
+++ L8_Error_&_Debugging/syntax.js
@@ -0,0 +1,17 @@
+const testingSyntax = () => {
+  console.log("I am inside a testing syntax function");
+};
+
+module.exports = testingSyntax;
+
+// Missing parenthesis in function call
+// console.log("Hello world";
+
+// Unclosed string literal
+// let msg = "Hello World;
+
+// Improper user of reserved keyword
+// let new = 7; // new is a reserved keyword
+
+// Incorrect cariable declaration
+// cionst myVar;
