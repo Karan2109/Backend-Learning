cb155a5c75ea56e4001efa5a762cae821ffb4889
Karan Singh <karans21.dev@gmail.com>
2025-05-19 17:36:49 +0530

    app.j for airbnb project

---
+++ L10 airbnb/app.js
@@ -0,0 +1,40 @@
+// External Modules
+const express = require("express");
+
+// Local Modules
+const userRouter = require("./routes/userRouters");
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log(req.url, req.method);
+  next();
+});
+
+app.use(express.urlencoded());
+
+app.use(userRouter);
+
+app.get("/host/add-home", (req, res, next) => {
+  res.send(`
+    <h1> Register on airbnb </h1>
+    <form action="/host/add-home" method="POST">
+        <input type="text" name="username" placeholder="Enter your name"/>
+        <input type="text" name="password" placeholder="Enter your password"/>
+        <input type="submit" value="Register"/>
+    </form>
+    `);
+});
+
+app.post("/host/add-home", (req, res, next) => {
+  console.log(req.body);
+  res.send(`
+    <h1> Registered successfully </h1>
+    <a href="/">Go to Home</a>  
+    `);
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port http://localhost:${PORT}`);
+});
