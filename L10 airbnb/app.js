ce8f6967790fe1f5cbe4efbcf000d107baec43ba
Karan Singh <karans21.dev@gmail.com>
2025-05-20 13:57:57 +0530

    main lserver file app.js

--- L10 airbnb/app.js
+++ L10 airbnb/app.js
@@ -1,9 +1,13 @@
+//core Module
+const path = require("path");
+
 // External Modules
 const express = require("express");
 
 // Local Modules
 const userRouter = require("./routes/userRouters");
 const hostRouter = require("./routes/hostRoutes");
+const rootDir = require("./utils/pathUtil");
 
 const app = express();
 
@@ -18,7 +22,7 @@ app.use(userRouter);
 app.use("/host", hostRouter);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found on airbnb</h1>");
+  res.status(404).sendFile(path.join(rootDir, "./views/404.html"));
 });
 
 const PORT = 5000;
